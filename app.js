a5423b1c5ee6d0e2870d4420380a6a44ad4a9312
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-09-28 17:57:45 +0200

    CORS and REST API

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-//to parsowanie jest ok dla contentu x-www-form-urlencoded czyli dla formularzy odpalanych na submit, jeśli
-// app.use(bodyParser.urlencoded());
-
-//parsowanie dla formatu JSON czyli headers: { 'Content-Type': 'application/json' }
 app.use(bodyParser.json());
+
+//ogólny middleware, który będzie nadawał każdemu przechodzącemu przez niego żądaniu
+//nagłówek z informacją odnośnie CORS-ów
+app.use((req, res, next) => {
+    //wymieniasz domeny dopuszczone - czyli zamiast wszystkich czyli * można wymienić np. tylko nasz FE
+    res.setHeader('Access-Controll-Allow-Origin', '*');
+    //wymieniasz dozwolone metody http
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    //wymieniasz dozwolone headersy
+    //jest grupa domyślnie dozwolonych
+    //tutaj wymieniamy 2 jeden dotyczący autoryzacji drugi że uzywamy json
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    //trzeba pamiętać żeby póścić przejście do kolejnego middleware-a
+    next();
+})
 
 app.use('/feed', feedRoutes);
 
