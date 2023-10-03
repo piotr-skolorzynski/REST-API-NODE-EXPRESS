81c7dba1362b1d6da06ae82ae0a4ea1cd7cd1c7d
piotr-skolorzynski <p.skolorzynski@gmail.com>
2023-10-03 10:50:32 +0200

    prepare app to be used by FE app

--- app.js
+++ app.js
@@ -6,21 +6,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
-//ogólny middleware, który będzie nadawał każdemu przechodzącemu przez niego żądaniu
-//nagłówek z informacją odnośnie CORS-ów
 app.use((req, res, next) => {
-    //wymieniasz domeny dopuszczone - czyli zamiast wszystkich czyli * można wymienić np. tylko nasz FE
     res.setHeader('Access-Control-Allow-Origin', '*');
-    //wymieniasz dozwolone metody http
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-    //wymieniasz dozwolone headersy
-    //jest grupa domyślnie dozwolonych
-    //tutaj wymieniamy 2 jeden dotyczący autoryzacji drugi że uzywamy json
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    //trzeba pamiętać żeby póścić przejście do kolejnego middleware-a
     next();
 })
 
 app.use('/feed', feedRoutes);
 
-app.listen(3000);
+app.listen(3001);
--- controllers/feed-controllers.js
+++ controllers/feed-controllers.js
@@ -1,18 +1,14 @@
 exports.getPosts = (req, res, next) => {
-    //przesyłamy kod statusu oraz metodą json() zamieniamy kontent w JSON-a
     res.status(200).json([
-        { title: 'First Post', content: 'This is the first post!' },
-        { title: 'Second Post', content: 'This is the second post!' },
-        { title: 'Third Post', content: 'This is the third post!' },
+        { title: 'First Post', content: 'This is the first post!', imageUrl: 'images/image1.png' },
+        { title: 'Second Post', content: 'This is the second post!', imageUrl: 'images/image1.png' },
+        { title: 'Third Post', content: 'This is the third post!', imageUrl: 'images/image1.png' },
     ])
 };
 
 exports.createPost = (req, res, next) => {
-    //dla przykładu dla odpowiedzi wykorzystane będzie co przychodzi z FE
     const title = req.body.title;
     const content = req.body.content;
-
-    //create post in db
 
     res.status(201).json({
         message: 'Post created successfully!',
